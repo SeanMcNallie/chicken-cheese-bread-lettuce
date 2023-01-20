1c3a80a2cdbbc59a5df2baf0d938e0913dfa6c8a
devin <devinelliotgomez@gmail.com>
2023-01-19 19:28:57 -0600

    added in new post routes, about to test to see if they will work

--- controllers/api/listRoutes.js
+++ controllers/api/listRoutes.js
@@ -1,3 +1,17 @@
 const router = require('express').Router();
 const { List } = require('../../models');
-const withAuth = require('../../utils/auth');
+
+router.post('/savedLists', async (req, res) => {
+    try {
+      const newList = await List.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newList);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+  
+  module.exports = router
--- public/js/shoppingList.js
+++ public/js/shoppingList.js
@@ -6,4 +6,25 @@ const newShoppingList = async function (event) {
     const store = document.getElementById("store").value; 
     const ingrediants = document.getElementById("description").value;
     const spending = document.getElementById("spending").value;
-}
+
+ if (title && occasion && store && ingrediants && spending) {
+        const response = await fetch(`/api/lists`, {
+          method: 'POST',
+          body: JSON.stringify({ title, occasion, store, ingrediants, spending }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+    
+        if (response.ok) {
+          document.location.replace('/saved');
+        } else {
+          alert('Failed to create project');
+        }
+      }
+    };
+
+    document
+    .getElementById("submission")
+    .addEventListener('click', newShoppingList);
+  
