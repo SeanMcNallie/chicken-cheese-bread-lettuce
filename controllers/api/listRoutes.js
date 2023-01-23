492ddf5563742955e3915babf53ac38389e6966a
devin <devinelliotgomez@gmail.com>
2023-01-23 17:54:47 -0600

    got the saved shopping lists elements of the website to work

--- controllers/api/listRoutes.js
+++ controllers/api/listRoutes.js
@@ -2,22 +2,20 @@ const router = require('express').Router();
 const { List } = require('../../models');
 
 router.post('/savedLists', async (req, res) => {
-    try {
-      const newList = await List.create({
-        ...req.body,
-        user_id: req.session.user_id,
-      });
-  
-      res.status(200).json(newList);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-
-    res.render('savedLists', {
-       newlist  
+  try {
+    const newList = await List.create({
+      ...req.body,
+      user_id: req.session.user_id,
     });
-  });
 
+    res.status(200).json(newList);
 
-  
-  module.exports = router
+    //res.render('savedLists', {
+      //newList,
+   // });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
