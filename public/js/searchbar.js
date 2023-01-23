7ec9e5bb3c06da4833477fb46b2b3b71f9978fbb
Easley <jessicaeasley@jessicas-air.attlocal.net>
2023-01-23 08:32:28 -0600

    removed unneeded code from searchbar js file

--- public/js/searchbar.js
+++ public/js/searchbar.js
@@ -1,23 +1,3 @@
-// const searchInput = document.querySelector('.input')
-
-// searchInput.addEventListener("input", (e) => {
-//     // inside, we will need to achieve a few things:
-//     // 1. declare and assign the value of the event's target to a variable AKA whatever is typed in the search bar
-//     let value = e.target.value
-
-//     // 2. check: if input exists and if input is larger than 0
-//     if (value && value.trim().length > 0){
-//         // 3. redefine 'value' to exclude white space and change input to all lowercase
-//          value = value.trim().toLowerCase()
-//         // 4. return the results only if the value of the search is included in the person's name
-//         // we need to write code (a function for filtering through our data to include the search input value)
-//     } else {
-//         // 5. return nothing
-//         // input is invalid -- show an error message or show no results
-
-//     }
-
-// }
   const searchInput = document.getElementById("search-input");
   const resultsDiv = document.getElementById("results");
   // Add event listener for enter key press
