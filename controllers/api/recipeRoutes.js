829aead1018c60e6d7be74957bc2569368ff1354
Easley <jessicaeasley@jessicas-air.attlocal.net>
2023-01-23 08:22:52 -0600

    updated results handlebars

--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -9,10 +9,10 @@ router.get('/results', async (req, res) => {
   // fetch recipes
   const EDAMAM_API_URL = 'https://api.edamam.com/api/recipes/v2?type=public&app_id=d23bf0e9&app_key=a93650b63c7c6bf742878f52d5349438';
   const search = await axios.get(
-    `${EDAMAM_API_URL}?apiKey=${process.env.EDAMAM_API_KEY}?apiID=${EDAMAM_ID}&number=${RECIPE_NUM}`
+    `${EDAMAM_API_URL}?apiKey=${process.env.EDAMAM_API_KEY}?apiID=${EDAMAM_ID}`
   );
   console.log('SEARCH?', search.data);
-  res.render('search', search.data);
+  res.render('results', search.data);
 });
 module.exports = router;
 
@@ -25,7 +25,7 @@ module.exports = router;
 // };
 
 
-module.exports = router;
+// module.exports = router;
 
 router.post('/', withAuth, async (req, res) => {
   try {
