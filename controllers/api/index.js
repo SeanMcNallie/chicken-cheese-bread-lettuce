5a3b3cbc57b4089f9bb191b8f503623952141da9
Easley <jessicaeasley@jessicas-air.attlocal.net>
2023-01-23 18:48:12 -0600

    updated searchbar .js to properly renmder search results

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const recipeRoutes = require('./recipeRoutes');
 const listRoutes = require('./listRoutes')
+// const resultsRoutes = require('./resultsRoutes');
 
 router.use('/users', userRoutes);
 router.use('/recipes', recipeRoutes);
-router.use('/lists', listRoutes)
+router.use('/lists', listRoutes);
+// router.use('./results', resultsRoutes)
 
 module.exports = router;
--- public/js/searchbar.js
+++ public/js/searchbar.js
@@ -1,5 +1,22 @@
   const searchInput = document.getElementById("search-input");
   const resultsDiv = document.getElementById("results");
+
+  const getRecipeItemHtml = (recipe) => {
+    return `
+    <a href='${recipe.url}' target="_blank" class='group'>
+      <div
+        class='w-full overflow-hidden rounded-md bg-green-600 '>
+       <h2 class='mt-1 mr-2 ml-2 text-center text-lg text-clip text-gray-900'>${recipe.label}</h2>
+        <img
+          src='${recipe.image}'
+          alt='${recipe.label}'
+          class='h-64 w-full object-cover object-center group-hover:opacity-75'/>
+        <p class='ml-2 mt-1 text-sm text-gray-900'>Source ${recipe.source} minutes</p>
+      </div>
+    </a>
+    `;
+  }
+
   // Add event listener for enter key press
   searchInput.addEventListener("keypress", event => {
     if (event.key === "Enter") {
@@ -25,16 +42,42 @@
         .then(data => {
           console.log(data)
           resultsDiv.innerHTML = "";
+          let itemsHtml = '';
           data.hits.forEach(result => {
-            const p = document.createElement("p");
-            p.innerText = result.recipe.label;
-            resultsDiv.appendChild(p);
+            const itemHtml = getRecipeItemHtml(result.recipe);
+            itemsHtml += itemHtml;
+            // const p = document.createElement("p");
+            // p.innerText = result.recipe.label;
+            // resultsDiv.appendChild(p);
           });
+
+          resultsDiv.innerHTML = itemsHtml;
         })
         .catch(error => console.error(error));
         
       }
   });
+
+
+/*
+
+<a href='{{recipe.sourceUrl}}' target="_blank" class='group'>
+            <div
+              class='w-full overflow-hidden rounded-md bg-green-600 '>
+             <h2 class='mt-1 mr-2 ml-2 text-center text-lg text-clip text-gray-900'>{{recipe.title}}</h2>
+              <img
+                src='{{recipe.image}}'
+                alt='Picture of food.'
+                class='h-64 w-full object-cover object-center group-hover:opacity-75'/>
+                  <p class='ml-2 mt-1 text-sm text-gray-900'>Ready in {{recipe.readyInMinutes}} minutes</p>
+                  <p class='ml-2 mt-1 text-sm text-gray-900'>Health Score: {{recipe.healthScore}}</p>
+            </div>
+          
+          
+          </a>
+
+*/
+
   // const apiKey = "a93650b63c7c6bf742878f52d5349438";
   // const apiId = "d23bf0e9";
   // // Encode the API key and secret as base64
